e9ac6403dfc38703b3ede3c317d825da53a7cce6
morgan button <morgan_button@yahoo.com>
2016-03-28 05:07:07 -0400

    added typing effect for homepage and included a index.js also used typed.js jquery plugin

---
+++ index.js
@@ -0,0 +1,10 @@
+setTimeout(function(){
+        $(".header").typed({
+            strings: ["Flowering Capabilities"],
+            typeSpeed: 110, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 0);
